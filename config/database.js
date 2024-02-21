90becb3319399cb64e6af67720210112ee1c67c0
Anamika Anamika <anamikaanamika@Anamikas-MacBook-Air.local>
2024-02-21 12:11:02 +0530

    postgres db config

--- config/database.js
+++ config/database.js
@@ -57,10 +57,10 @@ module.exports = ({ env }) => {
       connection: {
         connectionString: env("DATABASE_URL"),
         host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
+        port: env.int("DATABASE_PORT", 5433),
+        database: env("DATABASE_NAME", "kaizen"),
+        user: env("DATABASE_USERNAME", "kaizen"),
+        password: env("DATABASE_PASSWORD", "kaizen"),
         ssl: env.bool("DATABASE_SSL", false) && {
           key: env("DATABASE_SSL_KEY", undefined),
           cert: env("DATABASE_SSL_CERT", undefined),
