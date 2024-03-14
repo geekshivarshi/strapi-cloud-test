3f56d58cf58fa0d0298ac6a4095210a27ea7e879
Aanchal Goyal <aanchalg@geekyants.com>
2024-03-14 13:31:40 +0530

    popover components added

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -906,6 +906,8 @@ export interface ApiHeaderHeader extends Schema.SingleType {
     bestSellers: Attribute.JSON;
     specialOffers: Attribute.JSON;
     quickBuys: Attribute.JSON;
+    popoverImage: Attribute.Media;
+    imageLink: Attribute.Component<'elements.link'>;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
