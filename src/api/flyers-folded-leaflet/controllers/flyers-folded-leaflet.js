8be25c52f82567a76283275c365bc142e664b81e
Aanchal Goyal <aanchalg@geekyants.com>
2024-04-19 11:49:36 +0530

    flyers and leaflets schema added

---
+++ src/api/flyers-folded-leaflet/controllers/flyers-folded-leaflet.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * flyers-folded-leaflet controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::flyers-folded-leaflet.flyers-folded-leaflet');
---
+++ src/api/flyers-folded-leaflet/services/flyers-folded-leaflet.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * flyers-folded-leaflet service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::flyers-folded-leaflet.flyers-folded-leaflet');
