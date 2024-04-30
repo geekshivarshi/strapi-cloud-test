d258ca28c9a12b34420ee66629683b223e9df277
logeshwarand <47322586+thelogeshwaran@users.noreply.github.com>
2024-04-30 19:09:02 +0530

    add - single type - benefits of kazien

---
+++ src/api/benefits-of-kaizen/services/benefits-of-kaizen.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * benefits-of-kaizen service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::benefits-of-kaizen.benefits-of-kaizen');
