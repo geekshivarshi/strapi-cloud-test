2c8600cc66f5d371ecd5f0c0025afc16b8f82253
logeshwarand <47322586+thelogeshwaran@users.noreply.github.com>
2024-05-15 16:02:12 +0530

    kaize-promise-card

---
+++ src/api/kaizen-promisecard/routes/kaizen-promisecard.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * kaizen-promisecard router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::kaizen-promisecard.kaizen-promisecard');
---
+++ src/api/kaizen-promisecard/services/kaizen-promisecard.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * kaizen-promisecard service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::kaizen-promisecard.kaizen-promisecard');
