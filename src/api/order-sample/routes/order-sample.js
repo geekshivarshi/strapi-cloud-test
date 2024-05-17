7541c8ceaf7e7357f7be16b498bbef7e45346a78
logeshwarand <47322586+thelogeshwaran@users.noreply.github.com>
2024-05-17 16:35:16 +0530

    order-sample-pack

---
+++ src/api/order-sample/routes/order-sample.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * order-sample router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::order-sample.order-sample');
