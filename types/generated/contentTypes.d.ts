9c8304350ddeaf5b13b09e636e2aa0b8f0d276c2
logeshwarand <47322586+thelogeshwaran@users.noreply.github.com>
2024-10-03 15:10:56 +0530

    page-heading-how-it-works

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1451,6 +1451,7 @@ export interface ApiHowItWorkHowItWork extends Schema.CollectionType {
     myAccountCard: Attribute.Component<'block.my-account-card'>;
     seo: Attribute.Component<'shared.seo'>;
     pageCover: Attribute.Component<'block.page-cover'>;
+    pageHeading: Attribute.Component<'block.page-heading'>;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
