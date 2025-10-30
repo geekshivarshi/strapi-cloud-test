b4d5b89fcbe884e54b00bbe887ee05ea563343b7
Shivarshi <shivarshi@geekyants.com>
2025-10-30 15:34:29 +0530

    fix: minor changes

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -50,6 +50,7 @@ export interface BlockCategoryProductOverrides extends Schema.Component {
       true
     >;
     url: Attribute.String;
+    name: Attribute.String;
   };
 }
 
@@ -1018,19 +1019,11 @@ export interface SharedSeo extends Schema.Component {
   info: {
     displayName: 'seo';
     icon: 'search';
+    description: '';
   };
   attributes: {
-    metaTitle: Attribute.String &
-      Attribute.Required &
-      Attribute.SetMinMaxLength<{
-        maxLength: 60;
-      }>;
-    metaDescription: Attribute.String &
-      Attribute.Required &
-      Attribute.SetMinMaxLength<{
-        minLength: 50;
-        maxLength: 160;
-      }>;
+    metaTitle: Attribute.String & Attribute.Required;
+    metaDescription: Attribute.String & Attribute.Required;
     metaImage: Attribute.Media;
     metaSocial: Attribute.Component<'shared.meta-social', true>;
     keywords: Attribute.Text;
