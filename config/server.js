dfcf526a059f02e16ecde0000c36323e694eca64
Shivarshi <shivarshi@geekyants.com>
2025-10-31 14:33:13 +0530

    feat(server): add HTTP server options with extended timeout settings

--- config/server.js
+++ config/server.js
@@ -7,4 +7,10 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  http: {
+    serverOptions: {
+      timeout: 600000, // Set timeout to 10 minutes (600,000ms)
+      headersTimeout: 600000, // Optional: also set headers timeout
+    },
+  }
 });
